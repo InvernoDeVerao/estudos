39c102393eb77485ceeed398916bd5f873674734
InvernoDeVerão <erick.winter@escola.pr.gov.br>
2026-04-01 17:37:56 -0300

    lista 2

--- lista2.py
+++ lista2.py
@@ -2,6 +2,11 @@ NP = []
 dados = []
 
 while True:
-    dados.append = input("digite seu nome:")
-    dados.append = int(input("digite sua idade:"))
+    dados.append(input("digite seu nome:"))
+    dados.append(int(input("digite sua idade:")))
+    NP = dados[:]
     
+    continuar = input("quer continuar? [S/N]").lower().strip()
+    if "n" in continuar:
+        break
+
